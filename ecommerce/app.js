bfd37ce818c8a67e9b50596c27f5f808ed0f646d
Milena <milena.mihneva@gmail.com>
2020-05-13 17:27:42 -0600

    connecting to Mongodb using npm i mongoose

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config()
 
+
+
+// app const
+const app = express();
+
+// database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+})
+.then(() => console.log("Database Connected!"));
+
+//routes
 app.get("/", (req, res) => {
     res.send("Hello from Node!");
-
-
 });
 
 
-// define the port
+// defining the port
 const port = process.env.PORT || 8000
 
 app.listen(port, () => {
