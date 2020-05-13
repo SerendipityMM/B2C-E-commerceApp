77fdd1e72ce93192fafa5b6ec6f09265b113ffc5
Milena <milena.mihneva@gmail.com>
2020-05-13 17:06:55 -0600

    project setup, express server

---
+++ ecommerce/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+require('dotenv').config()
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node!");
+
+
+});
+
+
+// define the port
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log('Server is running on port ' + port );
+})
