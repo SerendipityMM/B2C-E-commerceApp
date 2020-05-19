b00ab1d9fe86ff94d79370d861068f462b845cdb
Milena <milena.mihneva@gmail.com>
2020-05-19 16:59:00 -0600

    working with POSTMAN

--- ecommerce/routes/user.js
+++ ecommerce/routes/user.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const { signup } = require('../controllers/user');
+const { userSignupValidator } = require('../validator');
  
 // creating a method
-router.post("/signup", signup);
+router.post("/signup", userSignupValidator, signup);
  
 
 module.exports = router;
--- ecommerce/validator/index.js
+++ ecommerce/validator/index.js
@@ -1,23 +1,24 @@
 // validate the user info/data for SignUp
 exports.userSignupValidator = (req, res, next) => {
     req.check('name', 'Name is required').notEmpty();
-    req.check('email', 'Email must be between 6 and 32 characters').notEmpty()
+    req.check('email', 'Email must be between 6 and 32 characters')
        .mathes(/.+\@.+\..+/)
        .withMessage('Email ust contain @')
        .isLength({
-           min: 6,
+           min: 4,
            max: 32
        });
        req.check('password', 'Password is required').notEmpty();
        req.check('password')
           .withMessage('Password must contain at least 6 characters')
           .isLength({ min: 6 })
-          .withMessage('Password must contain a number')
-          .mathes(/\d/);
+          .mathes(/\d/)
+          .withMessage('Password must contain a number');
+         
 
-          const erros = req.ValidationErrors();
-          if(erros) {
-              const  firstError = errors.map(error => error.message)[0];
+          const errors = req.ValidationErrors()
+          if(errors) {
+              const  firstError = errors.map(error => error.msg)[0];
               return res.status(400).json({error:firstError});
           }
 
