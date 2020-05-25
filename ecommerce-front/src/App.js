0a1af4983e9ac62db6beb8b55f1b9c8aac8f0979
Milena <milena.mihneva@gmail.com>
2020-05-25 16:49:15 -0600

    front end development

---
+++ ecommerce-front/src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+const App = () => (<div>Hellooooooo</div>);
+
+export default App;
---
+++ ecommerce-front/src/user/Signin.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Signin = () => <div>SignIn</div>;
+
+export default Signin;
